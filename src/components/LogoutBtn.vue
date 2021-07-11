41e33eb1433faaa1852bcac1a6cebbc89b448f70
nicolas <nicolas.kelepuris@gmail.com>
2021-07-10 22:45:00 -0300

    componentizados chat e botao de logout

---
+++ src/components/LogoutBtn.vue
@@ -0,0 +1,19 @@
+<template>
+  <form class="d-flex m-4">
+    <input class="float-left" type="button" value="Logout" @click="onLogout" />
+  </form>
+</template>
+
+<script>
+import { logout } from "../infrastructure/auth_service";
+export default {
+  methods: {
+    onLogout() {
+      logout();
+    },
+  },
+};
+</script>
+
+<style>
+</style>
