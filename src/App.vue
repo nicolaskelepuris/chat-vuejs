d36affdce316559e096e01a5cbd377c87dd6b2d2
nicolas <nicolas.kelepuris@gmail.com>
2021-07-14 15:04:40 -0300

    add loading screen

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,57 @@
 <template>
+  <VueElementLoading :active="isLoading" is-full-screen />
   <router-view />
 </template>
+
+<script>
+import VueElementLoading from "vue-element-loading";
+const axios = require('axios');
+export default {
+  data() {
+    return {
+      refCount: 0,
+      isLoading: false,
+    };
+  },
+  methods: {
+    setLoading(isLoading) {
+      if (isLoading) {
+        this.refCount++;
+        this.isLoading = true;
+      } else if (this.refCount > 0) {
+        this.refCount--;
+        this.isLoading = this.refCount > 0;
+      }
+    },
+  },
+  components: {
+    VueElementLoading,
+  },
+  created() {
+    axios.interceptors.request.use(
+      (config) => {
+        this.setLoading(true);
+        return config;
+      },
+      (error) => {
+        this.setLoading(false);
+        return Promise.reject(error);
+      }
+    );
+
+    axios.interceptors.response.use(
+      (response) => {
+        this.setLoading(false);
+        return response;
+      },
+      (error) => {
+        this.setLoading(false);
+        return Promise.reject(error);
+      }
+    );
+  },
+};
+</script>
 
 <style>
 #app {
--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 import App from './App.vue';
 import router from './router';
 import { createApp, h } from 'vue';
